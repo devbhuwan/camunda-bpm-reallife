e613eea58196fa8e9eb09933495902f4f688016c
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-30 23:48:47 +0400

    camunda-bpm-reallife

---
+++ bpmn-to-constant-generator/src/main/java/bpmntoconstant/generator/annotations/EnableBpmnMetaDataConstantGenerator.java
@@ -0,0 +1,10 @@
+package bpmntoconstant.generator.annotations;
+
+/**
+ * <p> </p>
+ *
+ * @author Bhuwan Prasad Upadhyay
+ */
+public @interface EnableBpmnMetaDataConstantGenerator {
+
+}
---
+++ bpmn-to-constant-generator/src/main/java/bpmntoconstant/generator/annotations/processors/EnableBpmnMetaDataConstantGeneratorProcessor.java
@@ -0,0 +1,29 @@
+package bpmntoconstant.generator.annotations.processors;
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
+import java.util.Set;
+
+import static bpmntoconstant.generator.annotations.processors.EnableBpmnMetaDataConstantGeneratorProcessor.ANNOTATIONS_ENABLE_BPMN_META_DATA_CONSTANT_GENERATOR;
+
+/**
+ * <p> </p>
+ *
+ * @author Bhuwan Prasad Upadhyay
+ */
+@SupportedAnnotationTypes({ANNOTATIONS_ENABLE_BPMN_META_DATA_CONSTANT_GENERATOR})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Processor.class)
+public class EnableBpmnMetaDataConstantGeneratorProcessor extends AbstractProcessor {
+
+    public static final String ANNOTATIONS_ENABLE_BPMN_META_DATA_CONSTANT_GENERATOR = "bpmntoconstant.generator.annotations.EnableBpmnMetaDataConstantGenerator";
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
