ec7e275d07cb6dc54d6c8a627e61df90b3080a2f
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-31 11:28:47 +0400

    camunda-bpm-reallife

---
+++ bpmn-to-constant-generator/src/test/resources/sources/GeneratorExample.java
@@ -0,0 +1,13 @@
+package sources;
+
+import bpmntoconstant.generator.annotations.EnableBpmnMetaDataConstantGenerator;
+
+/**
+ * <p> </p>
+ *
+ * @author Bhuwan Prasad Upadhyay
+ */
+@EnableBpmnMetaDataConstantGenerator
+public class GeneratorExample {
+
+}
---
+++ order-service/src/main/java/order/service/OrderServiceApplication.java
@@ -0,0 +1,11 @@
+package order.service;
+
+
+/**
+ * <p> </p>
+ *
+ * @author Bhuwan Prasad Upadhyay
+ */
+public class OrderServiceApplication {
+
+}
