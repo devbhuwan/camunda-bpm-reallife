ff17b2dfbbcac838c983f50443dd7d4b16d9a1da
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-09-11 00:16:50 +0400

    Camunda-Reallife

--- order-service/src/main/java/order/service/OrderServiceApplication.java
+++ order-service/src/main/java/order/service/OrderServiceApplication.java
@@ -2,12 +2,20 @@ package order.service;
 
 
 import bpmntoconstant.generator.annotations.EnableBpmnMetadataConstantGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 /**
  *
  * @author Bhuwan Prasad Upadhyay
  */
 @EnableBpmnMetadataConstantGenerator
+@SpringBootApplication
 public class OrderServiceApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 }
---
+++ order-service/src/test/java/order/service/OrderProcessTest.java
@@ -0,0 +1,55 @@
+package order.service;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.scenario.ProcessScenario;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Bhuwan Prasad Upadhyay
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = OrderServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Deployment(resources = {"bpmn/ORDER_PROCESS.bpmn"})
+public class OrderProcessTest {
+
+    @Mock
+    private ProcessScenario orderProcess;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private ProcessEngine processEngine;
+
+    @Rule
+    @ClassRule
+    public static ProcessEngineRule rule;
+
+    @PostConstruct
+    void initRule() {
+        rule = TestCoverageProcessEngineRuleBuilder.create(processEngine).build();
+    }
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void orderProcess() {
+
+    }
+
+}
